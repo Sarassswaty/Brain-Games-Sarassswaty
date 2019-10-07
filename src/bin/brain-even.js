f5afb6f102cbd290fddd0b6994163c2945dd583f
Sarasswaty <sarassswaty@gmail.com>
2019-10-07 21:20:49 +0300

    add brain-even.js

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { getRandomNum, askQuestion } from '..';
+
+getRandomNum();
+askQuestion();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import readlineSync from 'readline-sync';
 
-const userName = () => {
+export const userName = () => {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
   return name;
 };
 
-export default userName;
+export const getRandomNum = (max) => Math.floor(Math.random() * Math.floor(max));
+const checkEven = (num) => {
+  if (num % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
+export const askQuestion = () => {
+  const num = getRandomNum(101);
+  const correctAnswer = checkEven(num);
+  const question = `Questioin: ${num}`;
+  return { question, correctAnswer };
+};
