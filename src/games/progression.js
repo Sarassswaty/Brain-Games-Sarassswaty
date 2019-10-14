0d767e766318be20b8e3889d8709bef2431f07c8
Sarasswaty <sarassswaty@gmail.com>
2019-10-14 23:23:15 +0300

    aded isProgression

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,7 +4,14 @@ import gameStart from '..';
 
 const rules = 'What number is missing in the progression?';
 
-
+const isProgression = () => {
+  const progress = [0];
+  const diff = getRandomNum(8);
+  for (let i = 0; i <= 10; i += 1) {
+    progress.push(progress[i] + diff);
+  }
+  return progress;
+};
 
 
 const gameData = () => {
