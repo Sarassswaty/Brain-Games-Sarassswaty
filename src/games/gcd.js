d4ea289c6974d5cefc828d5ae5cf091d3c632419
Sarasswaty <sarassswaty@gmail.com>
2019-10-14 22:04:44 +0300

    aded fuction isEven

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,7 +4,16 @@ import gameStart from '..';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 
+const isEven = (a, b) => {
+  if (b === 0) {
+    return Math.abs(a);
+  }
+  return isEven(b, a % b);
+};
+
 const gameData = () => {
+  const a = getRandomNum(101);
+  const b = getRandomNum(101);
 
 };
 
