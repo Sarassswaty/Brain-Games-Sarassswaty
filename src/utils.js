83fe3c552d025b698eeb915a47d0c9be8652c2c4
Sarasswaty <sarassswaty@gmail.com>
2019-10-14 13:19:03 +0300

    aded function chooseChar

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,25 @@ import readlineSync from 'readline-sync';
 
 export const getRandomNum = (max) => Math.floor(Math.random() * Math.floor(max));
 
+export const characters = ['+', '-', '*'];
+
+export const chooseChar = (char) => {
+  let fn;
+  switch (char) {
+    case '+':
+      fn = (a, b) => a + b;
+      break;
+    case '-':
+      fn = (a, b) => a - b;
+      break;
+    case '*':
+      fn = (a, b) => a * b;
+      break;
+      // no default
+  }
+  return fn;
+};
+
 export const userName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
