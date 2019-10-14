6e277cb56e7a836ac96dbcebfcbcfd102eed8244
Sarasswaty <sarassswaty@gmail.com>
2019-10-14 17:20:04 +0300

    moved chooseChar to calc.js

--- src/utils.js
+++ src/utils.js
@@ -3,17 +3,6 @@ import readlineSync from 'readline-sync';
 
 export const getRandomNum = (max) => Math.floor(Math.random() * Math.floor(max));
 
-export const characters = ['+', '-', '*'];
-
-export const chooseChar = (a, b, char) => {
-  switch (char) {
-    case '+': return a + b;
-    case '-': return a - b;
-    case '*': return a * b;
-    // no default
-  }
-};
-
 export const userName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
