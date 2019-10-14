4592ad1e4df84ff16a682fb3226349da3532eb2c
Sarasswaty <sarassswaty@gmail.com>
2019-10-14 21:50:20 +0300

    aded import, export & basic func

---
+++ src/games/gcd.js
@@ -0,0 +1,11 @@
+import { getRandomNum } from '../utils';
+
+import gameStart from '..';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const gameData = () => {
+
+};
+
+export default () => gameStart(gameData, rules);
