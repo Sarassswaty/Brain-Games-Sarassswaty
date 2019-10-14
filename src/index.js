ff9f8984ec43edf4410a421b6c28fb0074a074c4
Sarasswaty <sarassswaty@gmail.com>
2019-10-14 13:24:22 +0300

    aded logic for brain-calc

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import readlineSync from 'readline-sync';
-import { getRandomNum, userName, checkEven } from './utils';
+
+import {
+  getRandomNum,
+  userName,
+  chooseChar,
+  characters,
+  checkEven,
+} from './utils';
 
 const getUserName = () => {
   console.log('Welcome to the Brain Games!');
@@ -29,6 +36,27 @@ const gameStart = () => {
 };
 
 const gameCalc = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('What is the result of the expression?\n');
+  const name = userName();
+  const count = 3;
+  for (let i = 0; i < count; i += 1) {
+    const char = characters[getRandomNum(3)];
+    const a = getRandomNum(101);
+    const b = getRandomNum(101);
+    const correctAnswer = chooseChar(char)(a, b);
+    const question = `Question: ${a} ${char} ${b}`;
+    console.log(question);
+    const answ = readlineSync.question('Your answer: ');
+    if (answ === correctAnswer) {
+      console.log('Correct');
+    } else {
+      console.log(`'${answ}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again, '${name}'!`);
+      return;
+    }
+  }
 
+  console.log(`Congratulations, ${name}!`);
 };
+
 export { getUserName, gameStart, gameCalc };
