644ae90611ec2aee5ac8eebef08a1772580b61c5
Sarasswaty <sarassswaty@gmail.com>
2019-10-16 14:31:13 +0300

    refactoring code progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,7 +6,7 @@ const rules = 'What number is missing in the progression?';
 
 const isProgression = () => {
   const progress = [0];
-  const diff = getRandomNum(8);
+  const diff = getRandomNum(10);
   for (let i = 0; i <= 10; i += 1) {
     progress.push(progress[i] + diff);
   }
@@ -17,14 +17,14 @@ const isProgression = () => {
 const gameData = () => {
   const prog = isProgression(getRandomNum(10));
   const index = getRandomNum(10);
-  const correctAnswer = prog[index];
+  const answer = prog[index];
   prog[index] = '..';
   let strProgr = '';
   for (let i = 0; i < prog.length; i += 1) {
     strProgr += `${prog[i]} `;
   }
   const question = `Question: ${strProgr}`;
-  return { question, correctAnswer };
+  return { question, answer };
 };
 
 export default () => gameStart(gameData, rules);
