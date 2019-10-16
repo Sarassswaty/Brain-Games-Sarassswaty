dccedaaf934b0ad0075ef87ae1bda277a2c9b449
Sarasswaty <sarassswaty@gmail.com>
2019-10-16 15:47:18 +0300

    aded function isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,6 +4,17 @@ import gameStart from '..';
 
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const isPrime = (n) => {
+  if( n === 1) 
+	return false;
+  for(let i = 2; i*i <= n; i += 1) { 
+	if (n % i === 0) {
+	  return false;
+	}
+  return true;
+  }
+};
+    
 const gameData = () => {
     const num = getRandomNum(101);
     const answer = isEven(num);
@@ -12,4 +23,3 @@ const gameData = () => {
   };
   
   export default () => gameStart(gameData, rules);
-  
