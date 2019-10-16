7a096d8a8e8bb5fd9e91faceec36f715f178c319
Sarasswaty <sarassswaty@gmail.com>
2019-10-16 16:02:51 +0300

    fixed problems prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,21 +5,22 @@ import gameStart from '..';
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (n) => {
-  if( n === 1) 
-	return false;
-  for(let i = 2; i*i <= n; i += 1) { 
-	if (n % i === 0) {
-	  return false;
-	}
-  return true;
+  if (n === 1) {
+    return 'no';
   }
+  for (let i = 2; i * i <= n; i += 1) {
+    if (n % i === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
 };
-    
+
 const gameData = () => {
-    const num = getRandomNum(101);
-    const answer = String(isPrime(num));
-    const question = `Question: ${num}`;
-    return { question, answer };
-  };
-  
-  export default () => gameStart(gameData, rules);
+  const num = getRandomNum(101);
+  const answer = String(isPrime(num));
+  const question = `Question: ${num}`;
+  return { question, answer };
+};
+
+export default () => gameStart(gameData, rules);
