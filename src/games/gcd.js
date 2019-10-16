ea606f1e6aa1d514da427e416a74d076df6dd778
Sarasswaty <sarassswaty@gmail.com>
2019-10-16 20:23:34 +0300

    fixed name function gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,17 +4,17 @@ import gameStart from '..';
 
 const getRules = 'Find the greatest common divisor of given numbers.';
 
-const isEven = (a, b) => {
+const isGcd = (a, b) => {
   if (b === 0) {
     return Math.abs(a);
   }
-  return isEven(b, a % b);
+  return isGcd(b, a % b);
 };
 
 const gameData = () => {
   const a = getRandomNum(101);
   const b = getRandomNum(101);
-  const answer = String(isEven(a, b));
+  const answer = String(isGcd(a, b));
   const question = `${a} ${b}`;
   return { answer, question };
 };
