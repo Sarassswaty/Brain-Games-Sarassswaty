2f579ca56c527f764b9f3750113747e7704c4578
Sarasswaty <sarassswaty@gmail.com>
2019-10-24 14:45:44 +0300

    fixed min max utils

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,1 @@
-import readlineSync from 'readline-sync';
-
-export const getRandomNum = (max) => Math.floor(Math.random() * Math.floor(max));
-
-export const userName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
+export default (min, max) => (Math.floor(Math.random() * ((max - min) + 1)) + min);
