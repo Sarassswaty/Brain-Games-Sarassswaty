a4f6a37cc9895531df2a9b61561edde369ffcaf7
Sarasswaty <sarassswaty@gmail.com>
2019-11-02 21:02:40 +0300

    fixed gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,18 +5,18 @@ import startGame from '..';
 const description = 'Find the greatest common divisor of given numbers.';
 
 const commonDivisor = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-  return commonDivisor(b, a % b);
+  if (a === 0) return b;
+  if (a < b) return commonDivisor(b, a);
+  return commonDivisor(a - b, b);
 };
 
+
 const gameData = () => {
-  const a = getRandomNum(1, 101);
-  const b = getRandomNum(1, 101);
-  const answer = String(commonDivisor(a, b));
+  const a = getRandomNum(1, 100);
+  const b = getRandomNum(1, 100);
   const question = `${a} ${b}`;
-  return [answer, question];
+  const answer = String(commonDivisor(a, b));
+  return [question, answer];
 };
 
 export default () => startGame(gameData, description);
