92ef72a8c45faf15c7adc3dd9f58ed6b720c3bd6
Sarasswaty <sarassswaty@gmail.com>
2019-11-02 21:34:01 +0300

    rewrited progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,30 +1,32 @@
 import getRandomNum from '../utils';
 
-import gameStart from '..';
+import startGame from '..';
 
-const getRules = 'What number is missing in the progression?';
+const description = 'What number is missing in the progression?';
 
-const isProgression = () => {
-  const progress = [0];
-  const diff = getRandomNum(10);
-  for (let i = 0; i <= 10; i += 1) {
-    progress.push(progress[i] + diff);
-  }
-  return progress;
+const progressionLength = 10;
+
+const getProgression = (startNumber, step) => {
+  const iter = (arr) => {
+    const len = arr.length;
+    if (len === progressionLength) {
+      return arr;
+    }
+    return iter(arr.concat(arr[len - 1] + step));
+  };
+  return iter([startNumber]);
 };
-
 
 const gameData = () => {
-  const prog = isProgression(getRandomNum(10));
-  const index = getRandomNum(10);
-  const answer = String(prog[index]);
-  prog[index] = '_';
-  let strProgr = '';
-  for (let i = 0; i < prog.length; i += 1) {
-    strProgr += `${prog[i]} `;
-  }
-  const question = `Question: ${strProgr}`;
-  return { question, answer };
+  const startNumber = getRandomNum(1, 50);
+  const step = getRandomNum(1, 10);
+  const hidenItemIndex = getRandomNum(1, 10);
+
+  const progression = getProgression(startNumber, step);
+  const answer = `${progression[hidenItemIndex]}`;
+  progression[hidenItemIndex] = '..';
+  const question = progression.join('  ');
+  return [question, answer];
 };
 
-export default () => gameStart(gameData, getRules);
+export default () => startGame(gameData, description);
