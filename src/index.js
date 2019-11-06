9909fdff70c822132e9df52d8151a4608f2a74d1
Sarasswaty <sarassswaty@gmail.com>
2019-11-06 17:52:48 +0300

    fixed errors final

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ export default (gameData, description) => {
   console.log(description);
   const userName = readlineSync.question('May i have your name? ');
   console.log(`Hello, ${userName}\n`);
-  const round = 3;
-  for (let i = 0; i < round; i += 1) {
+  const roundsCount = 3;
+  for (let i = 0; i < roundsCount; i += 1) {
     const [question, answer] = gameData();
     console.log(question);
     const userAnsw = readlineSync.question('Your answer: ');
     if (userAnsw === answer) {
       console.log('Correct');
     } else {
-      console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${answer}'`);
+      console.log(`'${userAnsw}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
