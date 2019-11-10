aa4c9ca9fc257a4d144e3b76fdfbd35a09896639
Sarasswaty <sarassswaty@gmail.com>
2019-11-10 15:33:06 +0300

    fixed name function

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,10 +4,10 @@ import startGame from '..';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const commonDivisor = (a, b) => {
+const commonGreatestDivisor = (a, b) => {
   if (a === 0) return b;
-  if (a < b) return commonDivisor(b, a);
-  return commonDivisor(a - b, b);
+  if (a < b) return commonGreatestDivisor(b, a);
+  return commonGreatestDivisor(a - b, b);
 };
 
 
@@ -15,7 +15,7 @@ const getData = () => {
   const a = getRandomNum(1, 100);
   const b = getRandomNum(1, 100);
   const question = `${a} ${b}`;
-  const answer = String(commonDivisor(a, b));
+  const answer = String(commonGreatestDivisor(a, b));
   return [question, answer];
 };
 
